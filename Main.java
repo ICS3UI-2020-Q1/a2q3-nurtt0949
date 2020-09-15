09b5d6896c4ff39add26664e734ac518e116c5ff
nurtt0949 <nurtt0949@wrdsb.ca>
2020-09-15 16:31:50 +0000

    Numbers in order

--- Main.java
+++ Main.java
@@ -1,6 +1,8 @@
+import java.util.Scanner;
+
 /**
- *
- * @author 
+ *This program determinds the order of 2 integers
+ * @author Tolga Nurtekin
  */
 public class Main {
 
@@ -9,7 +11,34 @@ public class Main {
    * @param args the command line arguments
    */
   public static void main(String[] args) {
-    // TODO code application logic here
-    
+    //create a Scanner for user input
+   Scanner input = new Scanner(System.in);
+
+   //ask user for an integers
+   System.out.println("Enter an integer");
+
+    //get integer from user
+  int integer1 = input.nextInt();
+  
+   //ask user to chose another integer
+  System.out.println("Enter another integer");
+
+  //get integer from user
+  int integer2 = input.nextInt();
+
+  //calculate greater number
+  if(integer1 < integer2){
+  
+  //tell user their numbers in ascending order
+  System.out.println("Your numbers in ascending order are " + integer1 + "," + integer2);
+
+ } else if (integer2 > integer1){
+
+  //tell user their number in ascending order
+  System.out.println("Your numbers in acending order are " + integer1 + "," +  integer2);
+  
+  
+  
+  } 
   }
 }
